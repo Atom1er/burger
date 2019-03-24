e0e8d140c593408938986bef580e7e78d61bc58f
Thomas <at93om@gmail.com>
2019-03-24 18:25:13 -0400

    Burger Done Heruko db

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,12 +1,19 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    database: 'burgers_db',
-    user: 'root',
-    password: 'Elvir@93'
+    host: 'ipobfcpvprjpmdo9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    database: 'egpt5wixzhcr236p',
+    port: 3306,
+    user: 'maemlh2qot70921k',
+    password: 'mnbbskio3ltztcfx'
 });
 
-connection.connect();
+connection.connect(function(err, res){
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+      }
+      console.log("connected as id " + connection.threadId);
+});
 
 module.exports = connection;
